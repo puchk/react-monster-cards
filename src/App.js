e456776b40b5e09a4e36a4f213fa303090891159
puchk <kpuchalski@gmail.com>
2018-05-15 15:22:53 -0700

    added jsonplaceholder api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { monsters } from './monsters';
 import './App.css';
 
 class App extends Component {
 	constructor(){
 		super();
 		this.state = {
-			monsters : monsters,
+			monsters : [],
 			searchfield : ''
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ monsters: users }));
 	}
 
 	onSearchChange = (event) => {
@@ -21,13 +26,17 @@ class App extends Component {
 		const filteredMonsters = this.state.monsters.filter(monster => {
 			return monster.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 		})
-		return (
-			<div className='tc'>
-				<h1>Monster Cards</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<CardList monsters={filteredMonsters}/>
-			</div>
-		);
+		if (this.state.monsters.length === 0) {
+			return <h1>Loading</h1>
+		} else {
+			return (
+				<div className='tc'>
+					<h1>Monster Cards</h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<CardList monsters={filteredMonsters}/>
+				</div>
+			);
+		}
 	}
 	
 }
