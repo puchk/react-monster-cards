72fa7442bee62195adef98f153cc074bc38cc2d8
puchk <kpuchalski@gmail.com>
2018-05-15 13:34:42 -0700

    created searchbox component, added state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { monsters } from './monsters';
 
-const App = () => {
-	return (
-		<div>
-			<h1>Monster Cards</h1>
-			<CardList monsters={monsters}/>
-		</div>
-	)
+class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			monsters : monsters,
+			searchfield : ''
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+	}
+
+	render() {
+		const filteredMonsters = this.state.monsters.filter(monster => {
+			return monster.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
+		return (
+			<div className='tc'>
+				<h1>Monster Cards</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<CardList monsters={filteredMonsters}/>
+			</div>
+		);
+	}
+	
 }
+
 
 export default App;
